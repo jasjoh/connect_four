46dcd3cc5cfd5c7266d07eda8dca0da86c120ad7
Jason Johnson <jasjoh@live.com>
2023-07-05 09:47:14 -0700

    WIP: implemented player switch

--- connect4.js
+++ connect4.js
@@ -118,9 +118,15 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  if (board.every(n => n !== null)) { return endGame() }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // if (currPlayer === 1) {
+  //   currPlayer = 2;
+  // } else {
+  //   currPlayer = 1;
+  // }
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
